1d2fcc14fa066caefec402dec923c65e77671664
Andrey <v.andrei.dmitrievich@gmail.com>
2025-04-11 15:18:14 +0300

    docstring added

--- app/deduplicate/deduplicate.py
+++ app/deduplicate/deduplicate.py
@@ -1,5 +1,21 @@
 class Deduplicate:
-    TTL = ...
+    """
+    Класс для дедубликации данных
+    Deduplicate:
+    class_attributes:
+        REDIS_TTL - Время жизни данных внутри redis
+        REDIS_CONNECT - Подключение к redis
+        DB_CONNECT - Подключение к бд (Clickhouse)
+    attributes: data - JSON данные о действии пользователя
+    methods: run() - Отбор дублирующих данных
+    __methods:
+        __get_hash() - Получение хэша данных
+        __check_redis() - Проверка данных внутри redis
+        __check_db() - Проверка данных внутри бд (Clickhouse)
+        __save_to_redis() - Сохранение данных в redis (сохранение хэша)
+        __save_to_db() - Сохранение данных в бд (Clickhouse)
+    """
+    REDIS_TTL = ...
     REDIS_CONNECT = ...
     DB_CONNECT = ...
 
