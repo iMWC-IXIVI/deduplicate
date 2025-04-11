6a9364c424b34d8cf17cb70931e503f63abe6664
Andrey <v.andrei.dmitrievich@gmail.com>
2025-04-11 15:22:08 +0300

    ref docs

--- app/deduplicate/deduplicate.py
+++ app/deduplicate/deduplicate.py
@@ -1,19 +1,21 @@
 class Deduplicate:
     """
-    Класс для дедубликации данных
-    Deduplicate:
+    Класс для дедубликации данных\n
+    Deduplicate:\n
     class_attributes:
-        REDIS_TTL - Время жизни данных внутри redis
-        REDIS_CONNECT - Подключение к redis
-        DB_CONNECT - Подключение к бд (Clickhouse)
-    attributes: data - JSON данные о действии пользователя
-    methods: run() - Отбор дублирующих данных
+        REDIS_TTL - Время жизни данных внутри redis\n
+        REDIS_CONNECT - Подключение к redis\n
+        DB_CONNECT - Подключение к бд (Clickhouse)\n
+    attributes:
+        data - JSON данные о действии пользователя\n
+    methods:
+        run() - Отбор дублирующих данных\n
     __methods:
-        __get_hash() - Получение хэша данных
-        __check_redis() - Проверка данных внутри redis
-        __check_db() - Проверка данных внутри бд (Clickhouse)
-        __save_to_redis() - Сохранение данных в redis (сохранение хэша)
-        __save_to_db() - Сохранение данных в бд (Clickhouse)
+        __get_hash() - Получение хэша данных\n
+        __check_redis() - Проверка данных внутри redis\n
+        __check_db() - Проверка данных внутри бд (Clickhouse)\n
+        __save_to_redis() - Сохранение данных в redis (сохранение хэша)\n
+        __save_to_db() - Сохранение данных в бд (Clickhouse)\n
     """
     REDIS_TTL = ...
     REDIS_CONNECT = ...
