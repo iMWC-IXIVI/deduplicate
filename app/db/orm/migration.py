16f74d911adaca799a6d3adaca081d464da5497e
Andrey <v.andrei.dmitrievich@gmail.com>
2025-04-12 14:51:34 +0300

    logger in migration

--- app/db/orm/migration.py
+++ app/db/orm/migration.py
@@ -4,30 +4,35 @@ import datetime
 from pathlib import Path
 from typing import Optional
 
-from core import settings
+from core import settings, LoggingService
 from db.orm import MiniORM
 
 
 migration_file = Path(__file__).resolve().parent.parent/'migrations'
+log = LoggingService()
 
 
 def get_connection() -> Optional[MiniORM]:
     """Подключение к базе данных"""
     try:
+        log.info_message('Начало подключения к БД')
         connect = MiniORM(port=settings.PORT, host=settings.HOST, username=settings.USERNAME, password=settings.PASSWORD, database=settings.DATABASE)
         connect.connect()
+        log.info_message('Успешное подключение к БД')
         return connect
     except Exception as e:
-        print(f'Исключение подключения к бд - {e}')
+        log.error_message(f'Исключение подключения к бд - {e}')
 
 
 def init_migration() -> None:
     """Инициализация первой таблицы (migration)"""
     try:
+        log.info_message('Начало проведения 0001_init_migration.sql')
         with open(migration_file/'0001_init_migration.sql', 'r', encoding='utf-8') as file:
             get_connection().connection.execute(file.read())
+        log.info_message('Миграция успешно применена')
     except Exception as e:
-        print(f'Исключение во время инициализации миграций - {e}')
+        log.error_message(f'Исключение во время инициализации миграций - {e}')
 
 
 def run_migration() -> None:
@@ -38,26 +43,32 @@ def run_migration() -> None:
     for file_name in dir_files:
 
         try:
+            log.info_message('Операция SELECT в таблицу migration началась')
             data = connect.select(['name_migration'], 'migrations', {'name_migration': f'{file_name}'})
+            log.info_message(f'Операция SELECT завершилась успешно, данные: {data}')
         except Exception as e:
-            print(f'MiniORM select не был выполнен - {e}')
+            log.error_message(f'MiniORM select не был выполнен - {e}')
             break
 
         if not data:
             try:
+                log.info_message('Начало проведения миграций')
                 with open(migration_file/file_name, 'r', encoding='utf-8') as file:
                     connect.connection.execute(file.read())
+                log.info_message('Миграции успешно проведены')
             except Exception as e:
-                print(f'MiniORM execute не выполнена миграция - {e}')
+                log.error_message(f'MiniORM execute не выполнена миграция - {e}')
                 break
 
             uuid_data = uuid.uuid4()
             dt_data = datetime.datetime.now()
 
             try:
+                log.info_message('Операция INSERT в таблицу migration началась')
                 connect.insert('migrations', ['id', 'name_migration', 'time_migration'], [(uuid_data, file_name, dt_data)])
+                log.info_message('Операция INSERT завершилась успешно')
             except Exception as e:
-                print(f'MiniORM insert не выполнил вставку данных - {e}')
+                log.error_message(f'MiniORM insert не выполнил вставку данных - {e}')
                 break
 
 
@@ -66,3 +77,4 @@ if __name__ == '__main__':
     # TODO Рассмотреть тему с ATOMIC REQUESTS в случае возбуждения исключений!!!
     init_migration()
     run_migration()
+    log.info_message('Успешное завершение всех миграций')
