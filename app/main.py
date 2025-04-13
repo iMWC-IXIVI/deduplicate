eb24689ffac01f88c77d4fa306418b82095549dc
Andrey <v.andrei.dmitrievich@gmail.com>
2025-04-13 08:14:54 +0300

    ref command uvi for test

--- app/main.py
+++ app/main.py
@@ -10,7 +10,3 @@ app = FastAPI(
     version='1.0.0'
 )
 app.include_router(router=router)
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info', reload=True)
