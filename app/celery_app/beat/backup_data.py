c8a0122134a52eb3d9b4d95aa451ca1be8962a9a
Andrey <v.andrei.dmitrievich@gmail.com>
2025-04-15 11:54:07 +0300

    log, try-except in beat

--- app/celery_app/beat/backup_data.py
+++ app/celery_app/beat/backup_data.py
@@ -2,23 +2,41 @@ from datetime import datetime
 
 from celery_app import app
 from db.orm import MiniORM
-from core import settings
+from core import settings, log
 
 
-connect = MiniORM()
-connect.connect()
-dt_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-
-def get_data():
-    yield from connect.connection.execute(f'SELECT raw_data FROM original WHERE created < \'{dt_now}\'')
+def get_data(connect, dt_now):
+    yield from connect.connection.execute(f'SELECT raw_data FROM original WHERE created < \'{dt_now.strftime("%Y-%m-%d %H:%M:%S")}\'')
 
 
 @app.task()
 def backup_data():
-    with open(settings.BASE_DIR/'backups'/f'{datetime.now().date()}.log', 'w', encoding='utf-8') as file:
-        for data in get_data():
-            file.write(f'{data}\n')
-    connect.connection.execute(f'DELETE FROM original WHERE created < \'{dt_now}\'')
-    connect.connection.execute(f'OPTIMIZE TABLE original FINAL')
+    try:
+        connect = MiniORM()
+        connect.connect()
+    except Exception as e:
+        log.error_message(f'Исключение во время подключения к clickhouse - {e}')
+        return 'Exception'
+
+    dt_now = datetime.now()
+
+    try:
+        log.info_message('Начинается резервное копирование')
+        with open(settings.BASE_DIR/'backups'/f'{dt_now.date()}.log', 'w', encoding='utf-8') as file:
+            for data in get_data(connect, dt_now):
+                file.write(f'{data}\n')
+        log.info_message('Резервное копирование завершено')
+    except Exception as e:
+        log.error_message(f'Исключение во время записи в backup-файл - {e}')
+        return 'Exception'
+
+    try:
+        log.info_message('Начинается удаление данных из clickhouse')
+        connect.connection.execute(f'DELETE FROM original WHERE created < \'{dt_now.strftime("%Y-%m-%d %H:%M:%S")}\'')
+        connect.connection.execute(f'OPTIMIZE TABLE original FINAL')
+        log.info_message('Завершение удаления данных из clickhouse')
+    except Exception as e:
+        log.error_message(f'Исключение во время удаление данных из clickhouse - {e}')
+        return 'Exception'
+
     return 'Данные успешно скопировались'
